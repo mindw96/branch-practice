5d807561a8a5f343831338b397f7dbb4d6bb8112
mindw96 <mindw96@naver.com>
2024-01-15 14:06:42 +0900

    feat: Add print(buzz)
    print buzz at multiple 5

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+if __name__=='__main__':
+    for i in range(1,16):
+        if i % 3 == 0:
+            print('fizz')
+        if i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
