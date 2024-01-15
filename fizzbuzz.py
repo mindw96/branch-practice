f00509f69412b42787228e2f870f629b28abbe8b
mindw96 <mindw96@naver.com>
2024-01-15 14:10:13 +0900

    feat: Add print fizzbuzz
    print fizzbuzz at multiple 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
 if __name__=='__main__':
     for i in range(1,16):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
             print('fizz')
-        if i % 5 == 0:
+        elif i % 5 == 0:
             print('buzz')
         else:
             print(i)
